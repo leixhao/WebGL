3b391d6c4ae8e2eac7fcfa623ed9d53602434fe1
leihao <hao.lei20@tendcloud.com>
2023-12-06 14:12:48 +0800

    请求地址

--- src/utils/requestN.ts
+++ src/utils/requestN.ts
@@ -17,7 +17,7 @@ export const isRelogin = { show: false };
 // 创建axios实例
 const service = axios.create({
     // axios中请求配置有baseURL选项，表示请求URL公共部分
-    baseURL: '/ecm-api/',
+    baseURL: '/ecm/ecm-api/',
     // 超时
     timeout: 10000,
     headers: {
